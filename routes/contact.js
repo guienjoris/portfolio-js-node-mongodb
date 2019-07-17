f448648124398da77e72c7f14cb479efe2917732
guienjoris <49984305+guienjoris@users.noreply.github.com>
2019-07-17 16:48:44 +0200

    back fini

--- routes/contact.js
+++ routes/contact.js
@@ -3,6 +3,16 @@ const router = express.Router();
 var passport = require('passport');
 let Contact = require ('../models/contact-model');
 
+var mailer = require('nodemailer');
+var smtpTransport= require('nodemailer-smtp-transport')
+var transport = mailer.createTransport(smtpTransport({
+    service: "gmail",
+    auth:{
+        user: "portfolioprod@gmail.com",
+        pass: "portfoliosimplon83."
+    }}));
+
+
 router.get('/contact' , (req,res) => {
     res.render('contact');  //L'adresse /contact renvoie vers la page contact.ejs
 })
@@ -29,6 +39,21 @@ router.post('/contact-post' , (req , res) =>{
         res.render( 'contact');
         console.log("Message bien envoyé!");
 
+    var mailContent = {
+        from: req.body.email,
+        to: 'guienjoris@gmail.com',
+        subject: req.body.title_contact ,
+        html: ` <p>Message de l'utilisateur:</p><p> ${req.body.textarea_contact}</p><p> De:</p><p> ${req.body.email}</p>`
+    }
+    transport.sendMail(mailContent, function(error, res){
+        if(error){
+            console.log("Erreur lors de l'envoi de l'email!");
+            console.log(error);
+        }else{
+            console.log('Mail envoyé avec succès!');
+        }
+        transport.close();
+    });
 })
 
 module.exports = router;
